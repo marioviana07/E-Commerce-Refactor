10cf950cc162323e975826cfb8785a6eebe4d6a1
marioviana07 <mario_viana_2005@yahoo.com>
2020-10-13 22:52:14 -0700

    Add code to index.js on models

--- models/Category.js
+++ models/Category.js
@@ -4,17 +4,24 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
+Category.init({
     // define columns
-  },
-  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    caterory_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+});
 
 module.exports = Category;
